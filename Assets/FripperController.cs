c7816585f1b65520c3a3e51726b5eff166172f2d
taishi kamiya <ishicca@gmail.com>
2020-04-21 21:06:55 +0900

    bugfix: touch interaction
    タッチしてから領域を超えてリリースしたときの挙動を修正

--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -12,8 +12,13 @@ public class FripperController : MonoBehaviour {
 	//弾いたときの傾き
 	private float flickAngle = -20;
 
-	// Use this for initialization
-	void Start () {
+    // Fripper Flag
+    private int leftFripperTouchId = -1;
+    private int rightFripperTouchId = -1;
+
+
+    // Use this for initialization
+    void Start () {
 		//HingeJointコンポーネント取得
 		this.myHingeJoint = GetComponent<HingeJoint>();
 
@@ -45,32 +50,52 @@ public class FripperController : MonoBehaviour {
         }
 
         //Touch
-        if(Input.touchCount > 0)
-        {
-            foreach(Touch t in Input.touches)
+//        if(Input.touchCount > 0)  //input.touchesで判定してるのでなくてもOK
+//        {
+
+            foreach (Touch t in Input.touches)
             {
+
                 //左半分を触った時
                 if(t.phase == TouchPhase.Began && t.position.x<Screen.width/2 && tag == "LeftFripperTag")
                 {
                     SetAngle(this.flickAngle);
+                    this.leftFripperTouchId = t.fingerId;
+                    Debug.Log("L:"+leftFripperTouchId);
                 }
                 //右半分を触った時
                 if (t.phase == TouchPhase.Began && t.position.x >= Screen.width / 2 && tag == "RightFripperTag")
                 {
                     SetAngle(this.flickAngle);
+                    this.rightFripperTouchId = t.fingerId;
+                    Debug.Log("R:"+rightFripperTouchId);
                 }
+
+
                 //左を離した時 エリア判定追加20200421
-                if (t.phase == TouchPhase.Ended && t.position.x < Screen.width / 2 && tag == "LeftFripperTag")
+                //エリア内処理
+                if (t.phase == TouchPhase.Ended && t.fingerId==this.leftFripperTouchId && tag == "LeftFripperTag")
                 {
+                    Debug.Log("Release L:" + t.fingerId);
                     SetAngle(this.defaultAngle);
+                    this.leftFripperTouchId = -1;
                 }
+
+
                 //右を離した時 エリア判定追加20200421
-                if (t.phase == TouchPhase.Ended && t.position.x >= Screen.width / 2 && tag == "RightFripperTag")
+                //エリア内処理
+
+                if (t.phase == TouchPhase.Ended && t.fingerId==this.rightFripperTouchId && tag == "RightFripperTag")
                 {
+                    Debug.Log("Release R:" + t.fingerId);
                     SetAngle(this.defaultAngle);
+                    this.rightFripperTouchId = -1;
+
                 }
+
+
             }
-        }
+       // }
 
 	}
 
